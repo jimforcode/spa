c9bf18cbe91b99692393cb2f032d8f1e626ca442
zj <zj@xiaoxiao.la>
2015-12-04 00:29:46 +0800

    add chat

---
+++ js/spa.chat.js
@@ -0,0 +1,31 @@
+/**
+ * Created by zengjin on 2015/12/3.
+ */
+spa.chat=(function(){
+
+    var  configMap={
+        main_html:String()+'<div style="padding: 1em;color: #fff;">hi,Jim well done!</div>',
+        settable_map:{}
+
+        },
+        stateMap={$container:null},
+        jqueryMap={}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
---
+++ js/spa.util.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zengjin on 2015/12/3.
+ */
